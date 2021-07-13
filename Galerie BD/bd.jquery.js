67df50d8e18c98599c67f0b844a1c16faba45572
Antony GIBBS <antony@cantoute.com>
2021-07-13 03:08:46 -0400

    version jQuery

---
+++ Galerie BD/bd.jquery.js
@@ -0,0 +1,39 @@
+// exemple de récuperation de valeur d'attribut title dans la console
+$('aside a').each(function () {
+  console.log($(this).attr('title'));
+});
+
+// sélectionner tous mes liens de vignette
+var navLinks = $('aside a');
+
+navLinks.on('click', function (event) {
+  // désactiver l'action par défaut d'un click sur un lien
+  event.preventDefault();
+
+  navLinks.filter('.active').removeClass('active');
+  $(this).addClass('active');
+
+  // récupérer l'attribut title du lien
+  // ou utiliser une valeur par défaut
+  let title = $(this).attr('title') || 'Sans titre';
+
+  let imgSrc = $(this).attr('href');
+
+  let imgAlt = $('img', this).attr('alt') || title;
+
+  // actualiser l'affichage
+  $('figure img').attr('src', imgSrc).attr('alt', imgAlt);
+  $('figcaption').text(title);
+
+  console.log('Viewing', title, imgSrc);
+
+  // bouclant sur toutes les vignettes remettre toutes les bordures à noir
+  // navLinks.css({
+  //   borderColor: 'black',
+  // });
+
+  // // affecter un contour rouge sur l'élément cliqué
+  // $(this).css({
+  //   borderColor: 'red',
+  // });
+});
