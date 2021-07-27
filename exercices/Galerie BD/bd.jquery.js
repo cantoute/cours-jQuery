ff16e07c7440ffc45571e4eb513f48ae4fe285df
Antony GIBBS <antony@cantoute.com>
2021-07-27 06:23:45 -0400

    db.jquery.js

--- exercices/Galerie BD/bd.jquery.js
+++ exercices/Galerie BD/bd.jquery.js
@@ -1,29 +1,31 @@
-// exemple de récuperation de valeur d'attribut title dans la console
-$('aside a').each(function () {
-  console.log($(this).attr('title'));
-});
+$(function () {
+  // exemple de récuperation de valeur d'attribut title dans la console
+  $('aside a').each(function () {
+    console.log($(this).attr('title'));
+  });
 
-// sélectionner tous mes liens de vignette
-var navLinks = $('aside a');
+  // sélectionner tous mes liens de vignette
+  var navLinks = $('aside a');
 
-navLinks.on('click', function (event) {
-  // désactiver l'action par défaut d'un click sur un lien
-  event.preventDefault();
+  navLinks.on('click', function (event) {
+    // désactiver l'action par défaut d'un click sur un lien
+    event.preventDefault();
 
-  navLinks.filter('.active').removeClass('active');
-  $(this).addClass('active');
+    navLinks.filter('.active').removeClass('active');
+    $(this).addClass('active');
 
-  // récupérer l'attribut title du lien
-  // ou utiliser une valeur par défaut
-  let title = $(this).attr('title') || 'Sans titre';
+    // récupérer l'attribut title du lien
+    // ou utiliser une valeur par défaut
+    let title = $(this).attr('title') || 'Sans titre';
 
-  let imgSrc = $(this).attr('href');
+    let imgSrc = $(this).attr('href');
 
-  let imgAlt = $('img', this).attr('alt') || title;
+    let imgAlt = $('img', this).attr('alt') || title;
 
-  // actualiser l'affichage
-  $('figure img').attr('src', imgSrc).attr('alt', imgAlt);
-  $('figcaption').text(title);
+    // actualiser l'affichage
+    $('figure img').attr('src', imgSrc).attr('alt', imgAlt);
+    $('figcaption').text(title);
 
-  console.log('Viewing', title, imgSrc);
+    console.log('Viewing', title, imgSrc);
+  });
 });
