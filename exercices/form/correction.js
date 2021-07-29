a00b828fd778611d98d6cc35d03b0bb6b553087b
Antony GIBBS <antony@cantoute.com>
2021-07-28 23:44:12 -0400

    Exo forms corrigé

---
+++ exercices/form/correction.js
@@ -0,0 +1,97 @@
+$(function () {
+  // 1. comme le javascript semble fonctionner, on efface le contenu de la div#msg
+  $('#msg').text('');
+
+  // 2. activer/désactiver l'édition de inputTitle
+  $('input[name=activeInputTitle]').on('change', function () {
+    var isChecked = $(this).prop('checked'); // Booléan (true|false)
+
+    console.log('checked', isChecked);
+
+    // if (isChecked) {
+    //   $('#inputTitle').prop('disabled', false);
+    // } else {
+    //   $('#inputTitle').prop('disabled', true);
+    // }
+    // version courte
+    $('#inputTitle').prop('disabled', !isChecked);
+
+    // Donner le focus au inputTitle et éviter un click
+    // mais uniquement si isChecked
+    // if (isChecked) {
+    //   $('#inputTitle').focus();
+    // }
+    // version courte
+    isChecked && $('#inputTitle').focus();
+  });
+
+  // 3. supprimer les espaces début/fin String.prototype.trim()
+  $('input[type=text]').on('change', function () {
+    var txt = $(this).val().trim();
+
+    /**
+     * Bonus :
+     * Si plus d'un espace, le remplacer par un seul espace
+     * Expression Rationnelle :
+     * \s : tout type d'espace \t (tabulation) \n (nouvelle ligne) \r (retour chariot)
+     * le plus ici signifie "au moins une fois"
+     * se traduit en : tout type d'espace 1 fois ou plus, remplacer par un espace ' '
+     */
+    txt = txt.replace(/\s+/g, ' ');
+
+    $(this).val(txt);
+  });
+
+  //4 Rendre le champ email obligatoire, aussi vérifier que l'email soit correct via isEmail()
+
+  $('input[name=lastName]').on('change', function () {
+    const txt = $(this).val().toUpperCase();
+    $(this).val(txt);
+  });
+
+  $('input[name=firstName]').on('change', function () {
+    const txt = ucFirst($(this).val());
+    $(this).val(txt);
+  });
+
+  // ça peut aussi s'écrire 'input[name$=Name]' (dont le name se termine en Name)
+  $('input[name=firstName], input[name=lastName]').on('keyup', function (e) {
+    $(this)
+      .closest('label') // Bonus: utiliser .trim() => pas uniquement des espaces par exemple
+      .toggleClass('error', !$(this).val().trim().length > 2);
+
+    // Juste pour info:
+    // Voici comment on teste si il y a au moins 2 caractères qui ne soient pas des espaces.
+    const minTwoChars = /[^\s]{2,}/.test($(this).val());
+  });
+  // on pourrait déclencher l'événement au chargement (donc une fois)
+  // .trigger('keyup');
+
+  $('input[name=email]').on('keyup change', function (e) {
+    const emailGood = isEmail($(this).val());
+    $(this).closest('label').toggleClass('error', !emailGood);
+  });
+
+  $('form').on('submit', function (e) {
+    e.preventDefault();
+
+    const good =
+      $('input[name=firstName]').val().length > 2 &&
+      $('input[name=lastName]').val().length > 2 &&
+      isEmail($('input[name=email]').val());
+
+    var msgOK = 'Nous avons bien reçu votre demande.';
+    var msgKO = 'Merci de vérifier votre saisie.';
+    var msg;
+
+    // if (good) {
+    //   msg = msgOK;
+    // } else {
+    //   msg = msgKO;
+    // }
+    // équivalent :
+    msg = good ? msgOK : msgKO;
+
+    $('#msg').text(msg);
+  });
+});
